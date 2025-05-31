fb7b6de6d6faa6943d01c231b52f53e4930427ba
Seizewells <167961728+Seizewells@users.noreply.github.com>
2025-06-01 00:08:18 +0700

    Создание основных файлов React приложения

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <h1 className="text-2xl font-bold text-center py-8">
+        SHM AQUA DEKOR
+      </h1>
+    </div>
+  )
+}
